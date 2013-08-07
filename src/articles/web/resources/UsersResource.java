c595d5bd2930c81b12d1e4c0e709fea2e204e7a9
gallyto <galina_hristova_@hotmail.com>
2013-08-07 14:09:20 +0300

    UsersResource

--- src/articles/web/resources/UsersResource.java
+++ src/articles/web/resources/UsersResource.java
@@ -1,5 +1,59 @@
 package articles.web.resources;
 
-public class UsersResource {
+import java.io.IOException;
 
+import javax.persistence.NoResultException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import articles.dao.UserDAO;
+import articles.model.User;
+
+@Path("")
+public class UsersResource {
+	@Context
+	ServletContext context;
+	
+	@POST
+	@Path("login")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response login(String username, String password,
+			@Context HttpServletResponse servletResponse,
+			@Context HttpServletRequest servletRequest) throws IOException, ServletException {
+		
+    	
+		UserDAO userDAO = new UserDAO();
+		try {
+			User user = userDAO.find(username, password);
+			
+			HttpSession session = servletRequest.getSession();
+			session.setAttribute("userId", user.getUserId());
+			return Response.ok(user.toString(), MediaType.APPLICATION_JSON).build();
+		} catch (NoResultException e) {
+			return Response.status(Response.Status.NOT_FOUND).entity("User not found ").build();
+		}
+	}
+	
+	@POST
+	@Path("logout")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response logout(@Context HttpServletResponse servletResponse,
+			@Context HttpServletRequest servletRequest) {
+		HttpSession session = servletRequest.getSession(false);
+		if (session != null) {
+			session.invalidate();
+			return Response.ok().build();
+			
+		}
+		return Response.status(Response.Status.UNAUTHORIZED).build();
+	}
 }
--- tests/articles/dao/userDAOTest.java
+++ tests/articles/dao/userDAOTest.java
@@ -1,6 +1,10 @@
 package articles.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +22,19 @@ public class userDAOTest {
 	
 	@Test
 	public void test() {
+		Properties prop = new Properties();
+		 
+    	try {
+               //load a properties file
+    		prop.load(new FileInputStream("WebContent/WEB-INF/config.properties"));
+
+	        //get the property value and print it out
+	            System.out.println(prop.getProperty("path"));
+
+	        } 
+	    catch (IOException ex) {
+	        ex.printStackTrace();
+	    }
 		User expectedUser = userDAO.find("admin", "123");
 		assertEquals(actualUser, expectedUser);
 		System.out.println(expectedUser.toString());
