ac196d4633690bd2cc8ab976cb0734210d6ea222
gallyto <galina_hristova_@hotmail.com>
2013-08-07 10:58:55 +0300

    users

--- src/articles/dao/UserDAO.java
+++ src/articles/dao/UserDAO.java
@@ -1,5 +1,29 @@
 package articles.dao;
 
-public class UserDAO {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
+import articles.model.User;
+
+public class UserDAO {
+	private static final String PERSISTENCE_UNIT_NAME = "UserPE";
+	private static EntityManagerFactory factory;
+	private EntityManager entityManager;
+	
+	public UserDAO() {
+		UserDAO.factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		this.entityManager = UserDAO.factory.createEntityManager();
+	}
+	
+	public User find(String username, String password) {
+		Query selectUserQuery = this.entityManager.createQuery("SELECT u FROM user WHERE u.username = :username and u.password = :password");
+		selectUserQuery.setParameter("username", username);
+		selectUserQuery.setParameter("password", password);
+		
+		User user = (User) selectUserQuery.getSingleResult();
+		
+		return user;
+	}
 }
--- src/articles/model/User.java
+++ src/articles/model/User.java
@@ -1,5 +1,47 @@
 package articles.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_articles")
 public class User {
+	@Id
+	@Column(name="userid")
+	private int userId;
+	private String username;
+	private String password;
+	
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username
+				+ ", password=" + password + "]";
+	}
 
 }
