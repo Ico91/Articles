2bc5225968f4fdc4ccfe4c00c9d0775b74c3a9c2
stinkybh <stkgstudio@gmail.com>
2013-08-09 10:55:15 +0300

    Ko?Ne!

---
+++ src/articles/web/resources/exception/ArticlesResourceException.java
@@ -0,0 +1,13 @@
+package articles.web.resources.exception;
+
+public class ArticlesResourceException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ArticlesResourceException() {
+		super();
+	}
+
+	public ArticlesResourceException(String message) {
+		super(message);
+	}
+}
