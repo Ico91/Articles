77a855cc12e1e889547316d4fcaded5e231cd9c0
gallyto <galina_hristova_@hotmail.com>
2013-08-13 16:18:30 +0300

    listener

--- src/articles/web/listener/SessionPathConfigurationListener.java
+++ src/articles/web/listener/SessionPathConfigurationListener.java
@@ -7,8 +7,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 import javax.ws.rs.core.Context;
 
 /**
@@ -17,7 +17,7 @@ import javax.ws.rs.core.Context;
  *
  */
 @WebListener
-public class SessionPathConfigurationListener implements HttpSessionAttributeListener, ServletContextListener {
+public class SessionPathConfigurationListener implements HttpSessionListener, ServletContextListener {
 	private static String path = "";
 	@Context
 	ServletContext context;
@@ -26,34 +26,6 @@ public class SessionPathConfigurationListener implements HttpSessionAttributeLis
 		return path;
 	}
 	
-	/**
-     * @see HttpSessionAttributeListener#attributeRemoved(HttpSessionBindingEvent)
-     */
-    public void attributeRemoved(HttpSessionBindingEvent event) {
-        // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see HttpSessionAttributeListener#attributeAdded(HttpSessionBindingEvent)
-     */
-    public void attributeAdded(HttpSessionBindingEvent event) {
-        // TODO Auto-generated method stub
-    	Properties properties = new Properties();
-		try {
-			properties.load(this.context.getResourceAsStream("WEB-INF/config.properties"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		path = properties.getProperty("path") + "/" + event.getValue();
-    }
-
-	/**
-     * @see HttpSessionAttributeListener#attributeReplaced(HttpSessionBindingEvent)
-     */
-    public void attributeReplaced(HttpSessionBindingEvent event) {
-        // TODO Auto-generated method stub
-    }
-
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 		// TODO Auto-generated method stub
@@ -64,6 +36,25 @@ public class SessionPathConfigurationListener implements HttpSessionAttributeLis
 	public void contextInitialized(ServletContextEvent event) {
 		// TODO Auto-generated method stub
 		this.context = event.getServletContext();
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+    	Properties properties = new Properties();
+		try {
+			properties.load(this.context.getResourceAsStream("WEB-INF/config.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		path = properties.getProperty("path") + "/";
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
