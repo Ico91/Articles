ae8135c2b3fcff88273edb74b5326980359fd3fb
stinkybh <stkgstudio@gmail.com>
2013-08-27 17:26:17 +0300

    AdministratorResource created

---
+++ src/articles/web/resources/administrator/AdministratorResource.java
@@ -0,0 +1,63 @@
+package articles.web.resources.administrator;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import articles.dao.UserDAO;
+import articles.model.User;
+import articles.model.dto.NewUserRequest;
+
+/**
+ * Class used to process all administrator requests
+ * 
+ * @author Krasimir Atanasov
+ * 
+ */
+@Path("")
+public class AdministratorResource {
+
+	private UserDAO userDAO;
+	
+	public AdministratorResource() {
+		this.userDAO = new UserDAO();
+	}
+
+	/**
+	 * Get information of all existing users
+	 * 
+	 * @return List of all users
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getUsers() {
+		return this.userDAO.getUsers();
+	}
+
+	/**
+	 * Create new user
+	 * 
+	 * @param userToAdd
+	 *            User to create
+	 * @return Response with status code 200 on success, 400 on error
+	 */
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addUser(NewUserRequest userToAdd) {
+		// TODO: Validations, unique username
+		return (this.userDAO.addUser(userToAdd)) ? Response.ok().build()
+				: Response.status(Status.BAD_REQUEST).build();
+	}
+
+	@Path("{id}")
+	public AdministratorSubResource getAdministratorSubResource() {
+		return new AdministratorSubResource();
+	}
+}
---
+++ src/articles/web/resources/administrator/AdministratorSubResource.java
@@ -0,0 +1,73 @@
+package articles.web.resources.administrator;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import articles.dao.UserDAO;
+import articles.model.User;
+
+public class AdministratorSubResource {
+
+	private UserDAO userDAO;
+
+	public AdministratorSubResource() {
+		this.userDAO = new UserDAO();
+	}
+
+	/**
+	 * Get user information based on specified id
+	 * 
+	 * @param id
+	 *            ID of the requested user information
+	 * @return User information
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUserById(@PathParam("id") int id) {
+		User userToReturn = this.userDAO.getUserById(id);
+
+		return (userToReturn != null) ? Response.ok(userToReturn,
+				MediaType.APPLICATION_JSON).build() : Response.status(
+				Status.NOT_FOUND).build();
+	}
+
+	/**
+	 * Update user information
+	 * 
+	 * @param id
+	 *            ID of the user to update
+	 * @param user
+	 *            New user information
+	 * @return Response with code 200 on success, 400 otherwise
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response update(@PathParam("id") int id, User user) {
+		// TODO: Validations
+
+		return (this.userDAO.updateUser(id, user)) ? Response.ok().build()
+				: Response.status(Status.NOT_FOUND).build();
+	}
+
+	/**
+	 * Delete user with specified id
+	 * 
+	 * @param id
+	 *            ID of the user to delete
+	 * @return Response with code 200 on success, 400 otherwise
+	 */
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response delete(@PathParam("id") int id) {
+		return (this.userDAO.deleteUser(id)) ? Response.ok().build() : Response
+				.status(Status.NOT_FOUND).build();
+	}
+}
