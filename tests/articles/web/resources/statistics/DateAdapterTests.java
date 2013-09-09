c6f32757f898e477846048424dd5abc2f957b79c
Hristo Danovski <hr.danovski@gmail.com>
2013-09-09 10:57:45 +0300

    Updated DateAdapterTests

--- tests/articles/web/resources/statistics/DateAdapterTests.java
+++ tests/articles/web/resources/statistics/DateAdapterTests.java
@@ -1,19 +1,36 @@
 package articles.web.resources.statistics;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collection;
 
 import javax.ws.rs.WebApplicationException;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import articles.web.resources.DateAdapter;
 
+@RunWith(value = Parameterized.class)
 public class DateAdapterTests {
 
+	private String date;
 	DateAdapter actualDate;
 
+	public DateAdapterTests(String date) {
+		this.date = date;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] data = new Object[][] { {"2013/08/32"}, {"2013/02/29"}, {""}, {"20/08/2013"}, {"2013-08-32"} };
+		return Arrays.asList(data);
+	}
+	
 	@Test
 	public void testDateAdapter() {
 		actualDate = new DateAdapter("2013/08/20");
@@ -35,27 +52,7 @@ public class DateAdapterTests {
 	}
 	
 	@Test (expected = WebApplicationException.class)
-	public void testInvalidDate() {
-		actualDate = new DateAdapter("2013/08/32");
-	}
-	
-	@Test (expected = WebApplicationException.class)
-	public void testNotLeapYear() {
-		actualDate = new DateAdapter("2013/02/29");
-	}
-	
-	@Test (expected = WebApplicationException.class)
-	public void testEmptyDate() {
-		actualDate = new DateAdapter("");
-	}
-
-	@Test (expected = WebApplicationException.class)
-	public void testWrongDate() {
-		actualDate = new DateAdapter("20/08/2013");
-	}
-	
-	@Test (expected = WebApplicationException.class)
-	public void testWrongDateFormat() {
-		actualDate = new DateAdapter("2013-08-32");
+	public void testWrongDates() {
+		actualDate = new DateAdapter(date);
 	}
 }
